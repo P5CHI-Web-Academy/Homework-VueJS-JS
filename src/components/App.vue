a5a683f1b36f91c25d5942174b27ad7e85c8bb4d
Vlad Dumbrava <vdumbrava@starlab.md>
2019-04-23 18:03:17 +0300

    try use vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,14 @@
 <template>
   <footer class="containter__footer">
-    <span class="containter__footer-title">footer</span>
+    <span class="containter__footer-title">{{ title }}</span>
   </footer>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'footer',
+    };
+  },
+};
+</script>
