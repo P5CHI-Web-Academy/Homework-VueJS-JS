754d087047025c1acbdb336272fbdba809369b25
Vlad Dumbrava <vdumbrava@starlab.md>
2019-04-23 12:44:08 +0300

    init with vue

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles/main.scss";
+
+//import vue.js
+import Vue from "vue";
+
+//import main component
+import App from "./components/App.vue";
+
+//render applications
+new Vue({
+  el: "#app",
+  render: h => h(App)
+});
+
+console.log("webpack work");
---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    }),
+    new VueLoaderPlugin()
+  ]
+};
