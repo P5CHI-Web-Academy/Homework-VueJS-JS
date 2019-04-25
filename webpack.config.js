80f0a81ad450483aeb22df0e87b690ce3cb8038c
Cristian <mlcristianv@gmail.com>
2019-04-25 17:55:39 +0300

    add dev server and html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-  	filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[chunkhash].js'
+  },
+  devServer:{
+    open:true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname, 'public/index.html')
+    })
+  ]
 };
