ac9e8a76b3de971f7f09a5033fe738124015017a
Cristian <mlcristianv@gmail.com>
2019-04-25 22:22:13 +0300

    add eslint and fix errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'vuetify/dist/vuetify.min.css'
-// import './assets/styles/main.scss';
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import App from './components/App.vue';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './components/App.vue'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
         }
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
@@ -51,7 +56,7 @@ module.exports = {
       },
       {
         test: /\.vue$/,
-        use: 'vue-loader'
+        use: ['vue-loader', 'eslint-loader']
       }
     ]    
   },
