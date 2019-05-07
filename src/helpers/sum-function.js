80b21b647a32543ec13ddfc8258a70476e6eb2f1
Cristian Cazacu <vladislav.tornea@gmail.com>
2019-05-07 22:45:20 +0300

    add function for infinity sum

--- src/helpers/sum-function.js
+++ src/helpers/sum-function.js
@@ -14,3 +14,6 @@ export const sumFnArrow = a => b => c => d =>
 // different approach
 // export const sumFnArrow = (a = 0) => (b = 0) => (c = 0) => (d = 0) =>
 //   a + b + c + d
+
+export const sumFnInfinity = n => i =>
+  Number.isInteger(i) ? sumFnInfinity(i + n) : n
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import '@style/style.scss'
 import Vue from 'vue'
 import Foo from '@component/App.vue'
 import Fibonacci from '@component/Fibonacci.vue'
-import { sumFn, sumFnArrow } from './helpers/sum-function'
+import { sumFn, sumFnArrow, sumFnInfinity } from './helpers/sum-function'
 
 console.log(sumFn(1)(2)(3)(4))
 console.log(sumFnArrow(1)(2)(3)())
+console.log(sumFnInfinity(2)(2)(2)(2)(2)(3)())
 
 Vue.component('fibonacci', Fibonacci)
 
